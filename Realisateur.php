a9da644270f2a1f075a3bda1a90ebba389b5733e
mika ramparany <migame.strass@gmail.com>
2023-02-16 11:32:26 +0100

    fonction construct d'une classe enfant + ajout "parent::__construct pour éviter de devoir reprendre les mêmes attributs de la fonction mère Personne

--- Realisateur.php
+++ Realisateur.php
@@ -1,8 +1,16 @@
-<?php 
+<?php
 
 //! Class enfant de PersonneMere.php
 
-class Realisateur extends PersonneMere {
+class Realisateur extends PersonneMere
+{
 
-    
+    private array $_AllFilm;
+
+
+    public function __construct(string $_Nom, string $_Prenom, string $_Sexe, $_DateNaissance)
+    {
+        parent::__construct($_Nom,  $_Prenom,  $_Sexe, $_DateNaissance);
+        $this->_AllFilm = [];
+    }
 }
