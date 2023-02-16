c35285ebfd9c94b83bf31b42bd4a74e869e300b8
mika ramparany <migame.strass@gmail.com>
2023-02-16 12:01:54 +0100

    Créatiion fonction add Film et Tableau Allfilms / pb : ne reconnait pas _Nom et _Prenom

--- Realisateur.php
+++ Realisateur.php
@@ -5,12 +5,43 @@
 class Realisateur extends PersonneMere
 {
 
-    private array $_AllFilm;
+    private array $_AllFilms;
 
 
     public function __construct(string $_Nom, string $_Prenom, string $_Sexe, $_DateNaissance)
     {
         parent::__construct($_Nom,  $_Prenom,  $_Sexe, $_DateNaissance);
-        $this->_AllFilm = [];
+        $this->_AllFilms = [];
     }
+
+    function get_AllFilms ()
+    {
+    return $this-> _AllFilms;
+    }
+
+    function set_AllFilms ($_AllFilms)
+    {
+    $this-> _AllFilms=$_AllFilms;
+    return $this;
+    }
+// créer une fonction qui ajoute les films du réalisateur 
+
+    public function AddFilm($Film){
+    $this->_AllFilms[] = $Film;
+    }
+
+    public function afficherFilm()
+    {
+        echo " Les films réalisés par ". $this ." : <br>";
+        foreach($this->_AllFilms as $Film)
+        {
+            echo $Film . "<br>";
+        }
+    }
+    public function __toString()
+    {
+        $result = $this->_Prenom . " " . $this->_Nom;
+        return $result;
+    }
+
 }
