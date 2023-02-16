0be7e5bb28c1f5b4dd8712256ca35d0cc4f69d27
mika ramparany <migame.strass@gmail.com>
2023-02-16 16:50:41 +0100

    rédaction Variables et Variable test

--- index.php
+++ index.php
@@ -9,3 +9,33 @@ spl_autoload_register(function ($class_name)
 //AfficherinfoFilm (titre, durée etc..)
 // Réalisteur (Nom, prénom, etc..)
 // Genre( )
+
+
+
+
+//REALISATEURS
+
+$Rea1 = new Realisateur (" David"," Lynch", "Homme", "20-01-1946");
+
+//GENRES 
+
+$G1 = new Genre ("Science-Fiction");
+$G2 = new Genre ("Action");
+
+//CASTINGS 
+// $CastFilm1 = New Casting ();
+// $CAFILM1 = new Casting()
+
+
+//FILMS 
+$FILM1=new Film ("Dune","06-02-1985", "137min", $Rea1, $G1 );
+$FILM2 =new Film ("Elephant Man", "08-04-1980", "124min" , $Rea1, $G1);
+
+
+
+//--------------------------------------
+
+// TESTS : 
+
+
+$FILM1-> afficherInfoFilm();
