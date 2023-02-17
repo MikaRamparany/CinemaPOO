8978e3322dc09dd869ce016125d02f7a886c22c0
mika ramparany <migame.strass@gmail.com>
2023-02-17 11:23:08 +0100

    rajout variables dans index

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $Rea3= new Realisateur ("Justin"," Lin", "Homme", "11-10-1971");
 
 $G1 = new Genre ("Science-Fiction");
 $G2 = new Genre ("Action");
-$G3 = new Genre ("Policier");
+$G3 = new Genre ("Thriller");
 
 //CASTINGS 
 // $CastFilm1 = New Casting ();
@@ -35,8 +35,10 @@ $FILM1=new Film ("Dune","06-02-1985", "137min", $Rea1, $G1 );
 $FILM2 =new Film ("Elephant Man", "08-04-1980", "124min" , $Rea1, $G1);
 $FILM3 = new Film ("Fast and Furious 4", "04-05-2009", "100min", $Rea3, $G2);
 $FILM4 = new Film ( "Fast and Furious 5", "29-04-2011", "130min", $Rea3, $G2);
-
-
+$FILM5 = new Film ( " Fast and Furious 6", "22-05-2013", " 130min", $Rea3, $G2);
+$FILM6 = new Film ( "Inception", "21-07-2010", "148min", $Rea2, $G3);
+$FILM7 = new Film ( "Transcendance", "25-06-2014" ,"120min",$Rea2, $G3);
+$FILM8 = new Film ( "Memento", "07-09-2000", "113min", $Rea2, $G3);
 //--------------------------------------
 
 // TESTS : 
