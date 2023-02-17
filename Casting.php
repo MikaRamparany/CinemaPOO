18b46fcd4324024002a6429ee49784b9ed9171d8
mika ramparany <migame.strass@gmail.com>
2023-02-17 13:49:30 +0100

    Ajout GETTERS and SETTERS sur la classe Casting

--- Casting.php
+++ Casting.php
@@ -1,12 +1,54 @@
 <?php
 
 class casting {
-    private  $_Film;
+    private   $_Film;
     private  $_Role;
-    private $_Acteur;
+    private  $_Acteur;
 
-    public function __construct ()
-
-
+    public function __construct ($_Film, $_Role, $_Acteur) 
     
+    {
+
+    $this -> _Film = $_Film;
+    $this -> _Role = $_Role;
+    $this -> $_Acteur = $_Acteur;
+    $this->_Acteur -> AjouterCasting($this);
+    $this -> _Role -> AjouterCasting($this);
+    $this -> _Film -> AjouterCasting($this);
+
+    }
+
+    // GETTERS AND SETTERS 
+
+    public function get_Film()
+    {
+        return $this -> _Film;
+    }
+    
+    public function set_Film($_Film)
+    {
+    $this -> _Film = $_Film;
+   
+    }
+    public function get_Role()
+    {
+        return $this -> _Role;
+    }
+    
+    public function set_Role($_Role)
+    {
+    $this -> _Role= $_Role;
+   
+    }
+    public function get_Acteur()
+    {
+        return $this -> _Acteur;
+    }
+    
+    public function set_Acteur($_Acteur)
+    {
+    $this -> _Acteur= $_Acteur;
+   
+    }
+
 }   
