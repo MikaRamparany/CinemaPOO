519bb1a5d29496518183dc47afbbc8b9a9f07c35
Mika RAMPARANY <migame.strass@gmail.com>
2023-02-20 02:17:04 +0100

    Rajout film réalisateur et role pour tester l'affichage de la filmographie d'un acteur

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ spl_autoload_register(function ($class_name)
 $Rea1 = new Realisateur (" David"," Lynch", "Homme", "20-01-1946");
 $Rea2 = new Realisateur ("Christopher", "Nolan", "Homme","30-07-1970");
 $Rea3= new Realisateur ("Justin"," Lin", "Homme", "11-10-1971");
-
+$Rea4 = new Realisateur ("David", "Twohy", "Homme", "18-10-1955");
 //GENRES 
 
 $G1 = new Genre ("Science-Fiction");
@@ -37,6 +37,9 @@ $FILM5 = new Film ( " Fast and Furious 6", "22-05-2013", " 130min", $Rea3, $G2);
 $FILM6 = new Film ( "Inception", "21-07-2010", "148min", $Rea2, $G3);
 $FILM7 = new Film ( "Transcendance", "25-06-2014" ,"120min",$Rea2, $G3);
 $FILM8 = new Film ( "Memento", "07-09-2000", "113min", $Rea2, $G3);
+$FILM9 = new Film ( "Les Chroniques de Riddick", "11-06-2004", "199min", $Rea4, $G2);
+
+
 //--------------------------------------
 
 // ROLES FILM 
@@ -46,15 +49,15 @@ $ROLFILM1DK = new Role ("Dr Kynes"); // Nb: joué par Max von Sydow
 
 $ROLFILM3DT = new Role ("Dominique Toreto"); // Nb: joué par Vin Diesel 
 $ROLFILM3OB = new Role ("Officier Brian"); // Nb: joué par Paul Walker 
-$ROLFILM4DT = new Role ("Domnique"); // Nb: joué par Vin Diesel 
+$ROLFILM4DT = new Role ("Dominique Toreto"); // Nb: joué par Vin Diesel 
 $ROLFILM4OB = new Role ("Officier Brian"); // Nb: joué par Paul Walker 
-$ROLFILM5DT = new Role ("Domnique"); // Nb: joué par Vin Diesel 
+$ROLFILM5DT = new Role ("Dominique Toreto"); // Nb: joué par Vin Diesel 
 $ROLFILM5OB = new Role ("Officier Brian"); // Nb: joué par Paul Walker 
 
 $ROLFILM6DC = new Role ("Dom Cobb"); // Nb: Di Caprio
 $ROLFILM6RF = new Role ("Rob Fischer"); // Nb:Cillian Murphy
 
-
+$ROLFILM9RK = new Role ("Riddick"); // Nb: Jouée par Vin Diesel
 // ACTEURS 
 
 $VD = new Acteur( "Vin", "Diesel", "Homme", "18-07-1967");
@@ -76,6 +79,9 @@ $CFLM3OB = new Casting ($FILM3, $ROLFILM3OB, $PW);
 
 $CFLM4DT = new Casting ($FILM4, $ROLFILM4DT, $VD);
 $CFLM4OB = new Casting ($FILM4, $ROLFILM4OB, $PW);
+$CFLM5DT = new Casting ($FILM5, $ROLFILM5DT, $VD);
+
+$CFLM9RK = new Casting ($FILM9, $ROLFILM9RK, $VD);
 
 
 // TESTS : 
@@ -92,5 +98,7 @@ echo  $Rea3 -> afficherFilmRea(); // Afficher les films du réalisateur
 echo $G2 -> afficherFilmparGenre();  // Afficher lees films par genre
 
 // echo $G3 -> afficherFilmparGenre(); 
+echo "<br> *********<br> <br>";
 
-echo $VD -> AfficherCastingActeur();
+
+echo $VD -> AfficherCastingActeur(); // "L'acteur Dominique Toreto 
